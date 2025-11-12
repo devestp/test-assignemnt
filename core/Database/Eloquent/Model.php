41995cb22e218c83ff69a2dcf4cfd67fcda92226
Sina Tahami <im.devestp@gmail.com>
2025-11-12 18:45:04 +0330

    chore(core/database): create Eloquent\Model base class

---
+++ core/Database/Eloquent/Model.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core\Database\Eloquent;
+
+use Illuminate\Database\Eloquent\Model as BaseModel;
+
+/**
+ * This class extends from the Laravel Model class and adds some auto-completion features to it.
+ *
+ * @method static static create(array $attributes)
+ */
+class Model extends BaseModel
+{
+    //
+}
