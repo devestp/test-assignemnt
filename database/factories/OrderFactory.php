0864e384d5a9203ddc72e4aa2fb11cece9608039
Sina Tahami <im.devestp@gmail.com>
2025-11-12 20:22:16 +0330

    chore(app/factories): add type to OrderFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\Order;
 use App\Models\User;
 use Domain\Enum\OrderState;
+use Domain\Enum\OrderType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,6 +19,7 @@ class OrderFactory extends Factory
             Order::USER_ID => User::factory(),
             Order::AMOUNT => fake()->randomNumber(),
             Order::PRICE => fake()->randomNumber(),
+            Order::TYPE => fake()->randomElement(OrderType::values()),
             Order::IDEMPOTENCY_TOKEN => fake()->dateTime()->getTimestamp(),
             Order::STATE => fake()->randomElement(OrderState::values()),
         ];
