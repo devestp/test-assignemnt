a084d620a4ea617aef5bfbaa02480d89a1c62b48
Sina Tahami <im.devestp@gmail.com>
2025-11-12 18:45:36 +0330

    chore(app/migrations): create orders migration

---
+++ database/migrations/2025_11_12_145413_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Enum\Table;
+use App\Models\Order;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create(Table::ORDERS->value, function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId(Order::USER_ID)
+                ->references('id')
+                ->on(Table::USERS->value)
+                ->restrictOnDelete();
+
+            $table->float(Order::AMOUNT, 6);
+
+            $table->float(Order::PRICE, 6);
+
+            $table->string(Order::IDEMPOTENCY_TOKEN);
+
+            // Used raw enum values to make sure future changes
+            // to the enum won't break this migration.
+            $table->enum(Order::STATE, ['pending', 'completed']);
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists(Table::ORDERS->value);
+    }
+};
