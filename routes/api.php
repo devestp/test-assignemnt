f9dcbf54822c5127abb88970bdd2211fd5738cd3
Sina Tahami <im.devestp@gmail.com>
2025-11-16 00:10:20 +0330

    feat(app/routes): register set order and get order book api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\GetOrderBookController;
+use App\Http\Controllers\Api\SetOrderController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::name('api.')->middleware('auth:sanctum')->group(function () {
+    Route::post('/order', SetOrderController::class)
+        ->name('order.set');
+
+    Route::get('/order-book', GetOrderBookController::class)
+        ->name('order-book');
+});
