3a18a5f6a60e5a1a52b2fb7b7ee24655c3153496
Sina Tahami <im.devestp@gmail.com>
2025-11-16 00:15:49 +0330

    test(models): create AdminTest

---
+++ tests/Feature/Models/AdminTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Feature\Models;
+
+use App\Models\Admin;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+use PHPUnit\Framework\Attributes\Group;
+use Tests\TestCase;
+
+#[Group('models')]
+class AdminTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_creates_with_only_required_values()
+    {
+        $email = 'test@test.com';
+        $password = Hash::make('password');
+
+        $admin = Admin::create([
+            Admin::EMAIL => $email,
+            Admin::PASSWORD => $password,
+        ]);
+
+        $this->assertEquals($email, $admin->email);
+        $this->assertTrue(Hash::check('password', $admin->password));
+    }
+}
