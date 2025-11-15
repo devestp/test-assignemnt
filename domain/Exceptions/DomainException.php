cc3d2fa06358d7936ec79d49b271697e7682e6a0
Sina Tahami <im.devestp@gmail.com>
2025-11-15 22:32:44 +0330

    chore(domain/exceptions): create base DomainException class

---
+++ domain/Exceptions/DomainException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Domain\Exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ * Base class for all domain related exceptions.
+ */
+abstract class DomainException extends Exception
+{
+    /**
+     * @param  string  $message  The message is required to make sure all exceptions have a meaningful message.
+     */
+    public function __construct(string $message, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $previous);
+    }
+}
