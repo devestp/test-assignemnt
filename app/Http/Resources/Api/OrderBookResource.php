39ef8a4cd86b3be9141d186b19c08576f07b2c0f
Sina Tahami <im.devestp@gmail.com>
2025-11-16 00:06:05 +0330

    chore(app/resources): create OrderBookResource

---
+++ app/Http/Resources/Api/OrderBookResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Domain\ValueObjects\OrderBook;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property-read OrderBook $resource
+ *
+ * @method static self make(OrderBook $orderBook)
+ */
+class OrderBookResource extends JsonResource
+{
+    private const ORDER_BOOK = 'orderBook';
+
+    private const SELL_ORDERS = 'sellOrders';
+
+    private const BUY_ORDERS = 'buyOrders';
+
+    public function toArray(Request $request): array
+    {
+        return [
+            self::ORDER_BOOK => [
+                self::BUY_ORDERS => OrderBookGroupResource::collection(
+                    $this->resource->getGroupedBuyOrders()->getGroups(),
+                ),
+                self::SELL_ORDERS => OrderBookGroupResource::collection(
+                    $this->resource->getGroupedBuyOrders()->getGroups(),
+                ),
+            ],
+        ];
+    }
+}
