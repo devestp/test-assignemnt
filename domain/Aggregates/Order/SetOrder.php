c4495c11e0ebab4753f0ba3bc750a4bc4e3c4e32
Sina Tahami <im.devestp@gmail.com>
2025-11-15 22:52:35 +0330

    chore(domain/aggregates): create Order/SetOrder aggregate

---
+++ domain/Aggregates/Order/SetOrder.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Domain\Aggregates\Order;
+
+use Domain\Entities\Order;
+use Domain\Entities\User;
+use Domain\Exceptions\NotEnoughCreditException;
+use Domain\Exceptions\UserNotAuthenticatedException;
+use Domain\Repositories\OrderRepository;
+use Domain\Repositories\UserRepository;
+use Domain\Services\AuthService;
+use Domain\ValueObjects\CreateOrderData;
+use Domain\ValueObjects\SetOrderData;
+use Illuminate\Support\HigherOrderTapProxy;
+
+/**
+ * Sets a new order in the system.
+ *
+ * This aggregate is safe to be used in transactions.
+ */
+class SetOrder
+{
+    public function __construct(
+        private AuthService $authService,
+        private OrderRepository $orderRepository,
+        private UserRepository $userRepository,
+    ) {}
+
+    /**
+     * @throws UserNotAuthenticatedException
+     * @throws NotEnoughCreditException
+     */
+    public function handle(SetOrderData $data): void
+    {
+        $user = $this->getCurrentUser();
+
+        if (! $this->doesUserHaveEnoughCredit($user, $data)) {
+            throw new NotEnoughCreditException($user);
+        }
+
+        $order = $this->createOrder($user, $data);
+
+        $this->subtractFromUserCredit($user, $data);
+
+        $this->matchWithOppositeOrder($order);
+    }
+
+    /**
+     * @throws UserNotAuthenticatedException
+     */
+    private function getCurrentUser(): User|HigherOrderTapProxy|null
+    {
+        return tap(
+            $this->authService->currentUser(),
+            // Negative space check
+            fn (?User $user) => $user ?? throw new UserNotAuthenticatedException,
+        );
+    }
+
+    private function doesUserHaveEnoughCredit(User $user, SetOrderData $data): bool
+    {
+        return $user->hasCredit(
+            $data->getAmount(),
+        );
+    }
+
+    private function createOrder(User $user, SetOrderData $setOrderData): Order
+    {
+        $createOrderData = new CreateOrderData(
+            userId: $user->getId(),
+            type: $setOrderData->getType(),
+            amount: $setOrderData->getAmount(),
+            price: $setOrderData->getPrice(),
+        );
+
+        // Because we know that the passed VO supports additional
+        // bag, we simply pass the additional data to the next VO
+        $createOrderData->additional($setOrderData->getAdditional());
+
+        return $this->orderRepository->create($createOrderData);
+    }
+
+    private function subtractFromUserCredit(User $user, SetOrderData $data): void
+    {
+        $user->subtractCredit($data->getAmount());
+
+        $this->userRepository->saveCredit($user);
+    }
+
+    private function matchWithOppositeOrder(Order $order): void
+    {
+        $matchedOrder = $this->orderRepository->getOldestMatchingOrderTo($order);
+
+        if (! is_null($matchedOrder)) {
+            $this->orderRepository->matchOrders(
+                first: $order,
+                second: $matchedOrder,
+            );
+        }
+    }
+}
---
+++ tests/Feature/Aggregates/Order/SetOrderTest.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace Tests\Feature\Aggregates\Order;
+
+use App\Models\Order;
+use App\Models\User;
+use Domain\Aggregates\Order\SetOrder;
+use Domain\Entities\User as UserEntity;
+use Domain\Enum\OrderType;
+use Domain\Exceptions\NotEnoughCreditException;
+use Domain\Repositories\OrderRepository;
+use Domain\Repositories\UserRepository;
+use Domain\Services\AuthService;
+use Domain\ValueObjects\CreateOrderData;
+use Domain\ValueObjects\SetOrderData;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\Group;
+use Tests\TestCase;
+
+#[Group('aggregates')]
+class SetOrderTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_throws_exception_if_user_does_not_have_enough_credit()
+    {
+        $user = $this->createUserWithoutCredit();
+        $this->mockAuthServiceThatReturnsUser($user);
+
+        $this->expectException(NotEnoughCreditException::class);
+
+        $this->setOrder(
+            $this->generateValidData(),
+        );
+    }
+
+    public function test_it_matches_buy_order_with_an_existing_sell_order()
+    {
+        $user = $this->createUserWithCredit();
+        $this->mockAuthServiceThatReturnsUser($user);
+        $data = $this->generateValidData(type: OrderType::BUY);
+        $this->mockUserRepositoryThatSubtractsUserCredit($user, $data);
+        $this->mockOrderRepositoryThatMatches($data);
+
+        $this->setOrder($data);
+    }
+
+    public function test_it_saves_buy_order_if_no_matching_order_exists()
+    {
+        $user = $this->createUserWithCredit();
+        $this->mockAuthServiceThatReturnsUser($user);
+        $data = $this->generateValidData(type: OrderType::BUY);
+        $this->mockUserRepositoryThatSubtractsUserCredit($user, $data);
+        $this->mockOrderRepositoryThatDoesntMatch($data);
+
+        $this->setOrder($data);
+    }
+
+    public function test_it_matches_sell_order_with_an_existing_buy_order()
+    {
+        $user = $this->createUserWithCredit();
+        $this->mockAuthServiceThatReturnsUser($user);
+        $data = $this->generateValidData(type: OrderType::SELL);
+        $this->mockUserRepositoryThatSubtractsUserCredit($user, $data);
+        $this->mockOrderRepositoryThatMatches($data);
+
+        $this->setOrder($data);
+    }
+
+    public function test_it_saves_sell_order_if_no_matching_order_exists()
+    {
+        $user = $this->createUserWithCredit();
+        $this->mockAuthServiceThatReturnsUser($user);
+        $data = $this->generateValidData(type: OrderType::SELL);
+        $this->mockUserRepositoryThatSubtractsUserCredit($user, $data);
+        $this->mockOrderRepositoryThatDoesntMatch($data);
+
+        $this->setOrder($data);
+    }
+
+    private function createUserWithoutCredit(): User
+    {
+        return User::factory()
+            ->hasCredit(0)
+            ->create();
+    }
+
+    private function createUserWithCredit(): User
+    {
+        return User::factory()
+            ->hasCredit(10000)
+            ->create();
+    }
+
+    private function mockAuthServiceThatReturnsUser(User $user): void
+    {
+        $this->mock(AuthService::class, function (MockInterface $mock) use ($user) {
+            $mock->shouldReceive('currentUser')
+                ->once()
+                ->andReturn($user->toEntity());
+        });
+    }
+
+    private function mockUserRepositoryThatSubtractsUserCredit(User $user, SetOrderData $data): void
+    {
+        $this->mock(UserRepository::class, function (MockInterface $mock) use ($user, $data) {
+            $userEntity = $user->toEntity();
+            $userEntity->subtractCredit($data->getAmount());
+
+            $mock->shouldReceive('saveCredit')
+                ->once()
+                ->with(Mockery::on(fn ($arg) => $this->isUserArgEqualTo($arg, $userEntity)));
+        });
+    }
+
+    private function mockOrderRepositoryThatMatches(SetOrderData $data): void
+    {
+        $this->mock(OrderRepository::class, function (MockInterface $mock) use ($data) {
+            $order = Order::factory()
+                ->type($data->getType())
+                ->price($data->getPrice())
+                ->amount($data->getAmount())
+                ->create()
+                ->toEntity();
+
+            $mock->shouldReceive('create')
+                ->once()
+                ->with(Mockery::on(fn ($arg) => $this->isCreateOrderDataArgEqualToSetOrderData($arg, $data)))
+                ->andReturn($order);
+
+            $matchingOrder = Order::factory()
+                ->type($data->getType()->opposite())
+                ->price($data->getPrice())
+                ->amount($data->getAmount())
+                ->create()
+                ->toEntity();
+
+            $mock->shouldReceive('getOldestMatchingOrderTo')
+                ->once()
+                ->with(Mockery::isSame($order))
+                ->andReturn($matchingOrder);
+
+            $mock->shouldReceive('matchOrders')
+                ->once()
+                ->with(Mockery::isSame($order), Mockery::isSame($matchingOrder));
+        });
+    }
+
+    private function mockOrderRepositoryThatDoesntMatch(SetOrderData $data): void
+    {
+        $this->mock(OrderRepository::class, function (MockInterface $mock) use ($data) {
+            $order = Order::factory()
+                ->type($data->getType())
+                ->price($data->getPrice())
+                ->amount($data->getAmount())
+                ->create()
+                ->toEntity();
+
+            $mock->shouldReceive('create')
+                ->once()
+                ->with(Mockery::on(fn ($arg) => $this->isCreateOrderDataArgEqualToSetOrderData($arg, $data)))
+                ->andReturn($order);
+
+            $mock->shouldReceive('getOldestMatchingOrderTo')
+                ->once()
+                ->with(Mockery::isSame($order))
+                ->andReturn(null);
+
+            $mock->shouldNotReceive('matchOrders');
+        });
+    }
+
+    private function generateValidData(?OrderType $type = null, ?string $idempotencyToken = null): SetOrderData
+    {
+        return (new SetOrderData(
+            type: $type ?? OrderType::BUY,
+            amount: 10,
+            price: 10,
+        ))->additional('idempotencyToken', $idempotencyToken ?? Str::uuid());
+    }
+
+    private function setOrder(SetOrderData $data): void
+    {
+        /** @var SetOrder $aggregate */
+        $aggregate = resolve(SetOrder::class);
+
+        $aggregate->handle($data);
+    }
+
+    private function isCreateOrderDataArgEqualToSetOrderData(CreateOrderData $arg, SetOrderData $setOrderData): bool
+    {
+        return $arg->getAmount() === $setOrderData->getAmount() &&
+            $arg->getType() === $setOrderData->getType() &&
+            $arg->getPrice() === $setOrderData->getPrice() &&
+            $arg->getAdditional() == $setOrderData->getAdditional();
+    }
+
+    private function isUserArgEqualTo(UserEntity $arg, UserEntity $user): bool
+    {
+        return $arg->getId() === $user->getId() &&
+            $arg->getCredit() === $user->getCredit() &&
+            $arg->getEmail() === $user->getEmail();
+    }
+}
