8a74a21715474f33fea3a840e506951994823269
Sina Tahami <im.devestp@gmail.com>
2025-11-16 18:02:42 +0330

    chore(app/factories): use BigDecimal & add new states in OrderBookFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Order;
 use App\Models\User;
+use Brick\Math\BigDecimal;
 use Domain\Enum\OrderState;
 use Domain\Enum\OrderType;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -56,12 +57,12 @@ class OrderFactory extends Factory
         });
     }
 
-    public function amount(int $amount): self
+    public function amount(int|float|BigDecimal $amount): self
     {
         return $this->state([Order::AMOUNT => $amount]);
     }
 
-    public function price(int $price): self
+    public function price(int|float|BigDecimal $price): self
     {
         return $this->state([Order::PRICE => $price]);
     }
@@ -74,5 +75,23 @@ class OrderFactory extends Factory
     public function pending(): self
     {
         return $this->state([Order::STATE => OrderState::PENDING]);
+    }
+
+    public function priceBetween(int|float|BigDecimal $min, int|float|BigDecimal $max): self
+    {
+        return $this->state(function () use ($min, $max) {
+            return [
+                Order::PRICE => fake()->randomFloat(min: $min, max: $max),
+            ];
+        });
+    }
+
+    public function amountBetween(int|float|BigDecimal $min, int|float|BigDecimal $max): self
+    {
+        return $this->state(function () use ($min, $max) {
+            return [
+                Order::AMOUNT => fake()->randomFloat(min: $min, max: $max),
+            ];
+        });
     }
 }
