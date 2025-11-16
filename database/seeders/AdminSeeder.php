e8a5c8ff8cbdb73bfe068731ba1b28e182482e61
Sina Tahami <im.devestp@gmail.com>
2025-11-16 18:09:50 +0330

    chore(app/seeders): create AdminSeeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Admin::create([
+            Admin::EMAIL => 'admin@admin.com',
+            Admin::PASSWORD => Hash::make('password'),
+        ]);
+    }
+}
