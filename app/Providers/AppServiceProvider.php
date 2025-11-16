eb0e9f089116b5b1076e60ebac4278db132da7ac
Sina Tahami <im.devestp@gmail.com>
2025-11-16 18:09:24 +0330

    chore(app/providers): add db listen callback to AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Uncomment to see what database select queries are being
+        // called. Useful for cache checks and query counts.
+        //        DB::listen(function($query) {
+        //            if (Str::contains($query->sql, 'update')) {
+        //                Log::debug(
+        //                    'select statement',
+        //                    context: [ 'sql' => $query->sql, 'bindings' => $query->bindings ],
+        //                );
+        //            }
+        //        });
     }
 }
